85f520d693e96ab450ff13b39063776268d19610
alicaptain <aligulerworks@gmail.com>
2022-06-19 19:34:19 +0300

    Seems Working

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 
 const adjuster = document.getElementById('adjuster');
 
+let meUpped = 0;
+let result = 0;
 
 adjuster.addEventListener('click', counter);
 
@@ -12,11 +14,12 @@ let PseudoBefore = window.getComputedStyle(adjuster, '::before');
 let meUp = PseudoBefore.getPropertyValue('content');
 
 function counter() {
+   meUpped = meUpped+1;
+   result = `'`+meUpped+`'`;
 
-   let meUpped = 36;
 
-   document.querySelector(':root').style.setProperty('--contenter', meUpped);
-
+   document.querySelector(':root').style.setProperty('--contenter', result);
+   return;
 
 }
 
