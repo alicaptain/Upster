52c0ce8a19e407c9e1e5aad34c9577cd45f88918
alicaptain <aligulerworks@gmail.com>
2022-06-19 19:57:15 +0300

    Growing Effect Implemented

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const adjuster = document.getElementById('adjuster');
 
 let meUpped = 0;
 let result = 0;
+let meGrow = 100;
+let meGrown;
 
 adjuster.addEventListener('click', counter);
 
@@ -15,10 +17,15 @@ let meUp = PseudoBefore.getPropertyValue('content');
 
 function counter() {
    meUpped = meUpped+1;
-   result = `'`+meUpped+`'`;
+   meGrow = meGrow+3;
 
+   result = `'`+meUpped+`'`;
+   meGrown = `scale(${meGrow}%)`;
 
    document.querySelector(':root').style.setProperty('--contenter', result);
+
+   document.querySelector(':root').style.setProperty('--grower', meGrown);
+
    return;
 
 }
