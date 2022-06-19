0b442d49dfd119180fb789c3d3257ecb9045e917
alicaptain <aligulerworks@gmail.com>
2022-06-19 19:25:50 +0300

    Just Another Step

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 'use strict';
 
+const adjuster = document.getElementById('adjuster');
+
+
+adjuster.addEventListener('click', counter);
+
+
+
+let PseudoBefore = window.getComputedStyle(adjuster, '::before');
+
+let meUp = PseudoBefore.getPropertyValue('content');
+
+function counter() {
+
+   let meUpped = 36;
+
+   document.querySelector(':root').style.setProperty('--contenter', meUpped);
+
+
+}
 
